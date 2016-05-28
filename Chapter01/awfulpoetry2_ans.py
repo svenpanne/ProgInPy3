b6b79099cb07c7ba595fc747dc47e8ad16d0780f
Sven Panne <svenpanne@gmail.com>
2016-05-28 19:23:48 +0200

    Added awful poetry excercises.

---
+++ Chapter01/awfulpoetry2_ans.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+import random
+import sys
+
+articles = ["the", "a", "another", "her", "his"]
+subjects = ["cat", "dog", "horse", "man", "woman", "boy", "girl"]
+verbs = ["sang", "ran", "jumped", "said", "fought", "swam", "saw",
+         "heard", "felt", "slept", "hopped", "hoped", "cried",
+         "laughed", "walked"]
+adverbs = ["loudly", "quietly", "quickly", "slowly", "well", "badly",
+           "rudely", "politely"]
+
+lines = 5
+if len(sys.argv) > 1:
+    try:
+        temp = int(sys.argv[1])
+        if 1 <= temp <= 10:
+            lines = temp
+        else:
+            print("lines must be 1-10 inclusive")
+    except ValueError:
+        print("usage:", sys.argv[0], "[lines]")
+
+while lines:
+    article = random.choice(articles)
+    subject = random.choice(subjects)
+    verb = random.choice(verbs)
+    if random.randint(0, 1) == 0:
+        print(article, subject, verb)
+    else:
+        adverb = random.choice(adverbs)
+        print(article, subject, verb, adverb)
+    lines -= 1
